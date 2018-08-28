70ca3654860dba966413aaa59613e4bee3f4c800
annika <annikaslund@gmail.com>
2018-08-27 20:50:50 -0700

    finish #next_turn, add #one_side_empty?

--- mancala/lib/board.rb
+++ mancala/lib/board.rb
@@ -43,11 +43,18 @@ class Board
       end
     end
 
+    next_turn(cup_idx)
     render
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -59,6 +66,8 @@ class Board
   end
 
   def one_side_empty?
+    side1 = @cups[0..6]
+    side2 = @cups[7..13]
   end
 
   def winner
