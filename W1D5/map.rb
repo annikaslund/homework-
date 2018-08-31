31090f617d5633b493fd672695262b06dacb917a
annika <annikaslund@gmail.com>
2018-08-31 07:19:28 -0700

    finish LRU cache

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -1,16 +1,21 @@
 class Map
   def initialize
+    @stack_stor = []
   end
 
   def set(key, value)
+
   end
 
   def get(key)
+
   end
 
   def delete(key)
+
   end
 
   def show
-  end 
+    
+  end
 end
